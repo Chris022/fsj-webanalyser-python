32ab90714da4e61643ae37a5feed66e7fb7e822f
Christoph <christoph@debian>
2022-09-06 21:02:07 +0200

    :lipstick: recreate ui with propper alpine-js

---
+++ public/analyse_view.js
@@ -0,0 +1,10 @@
+function analyse_view_init(){
+    
+    return {
+        url: "",
+        start_analyse: function (){
+
+            console.log(this.url)
+        }
+    }
+}
---
+++ public/loading_view.js
@@ -0,0 +1,44 @@
+async function loading_view_init(){
+    handle_status()
+    setInterval(() => {
+        handle_status()
+    }, 5000);
+}
+
+async function handle_status(){
+    let status = await check_analysing_status();
+
+    if(status == "not_analysed"){
+        alert("Wohaaa... That url is not analysed yet. Please first analyse it!");
+        window.location="/analyse_view.html";
+    }
+    if(status == "still_analysing"){
+        //do nothing
+    }
+    if(status == "finished_analysing"){
+        window.location="/results_view.html";
+    }
+}
+
+async function check_analysing_status(){
+    let url = "dispoman.com"
+
+    const response = await fetch("/results", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"url":url}) // body data type must match "Content-Type" header
+    }).then(result => result.json());
+
+    if(response.type=="SUCCESS"){
+        return "finished_analysing";
+    }
+    if(response.data == "not_analysed"){
+        return "not_analysed";
+    }
+    if(response.data == "still_analysing"){
+        return "still_analysing";
+    }
+    return "other_error";
+}
